e432c1ed38a8056c57d5db16bf2de7d222bd4da8
JoseDavidMora <josedavidmoramagana@gmail.com>
2021-02-02 18:54:18 -0600

    script que crea el diccionario de datos

---
+++ Creacion BD/s-05-crea-diccionario-datos.sql
@@ -0,0 +1,16 @@
+--@Autores: Jaime Garcia Frnacisco
+--     	    Mora Magaña Jose David Divad
+--@Fecha:   01/02/2021
+--@Descripcion: Creacion del diccionario de datos.
+
+connect sys/systemproy as sysdba
+
+@?/rdbms/admin/catalog.sql
+@?/rdbms/admin/catproc.sql
+@?/rdbms/admin/utlrp.sql
+
+connect system/systemproy
+
+@?/sqlplus/admin/pupbld.sql
+
+
