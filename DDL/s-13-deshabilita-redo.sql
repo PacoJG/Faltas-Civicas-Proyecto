922835e89c1303a717da7a58dc282cb0245265d8
PacoJG <ing.franciscojaimegarcia@gmail.com>
2021-02-03 15:51:58 -0600

    Deshabilitar datos redo

--- DDL/s-13-deshabilita-redo.sql
+++ DDL/s-13-deshabilita-redo.sql
@@ -2,3 +2,11 @@
 --     	    Mora Magaña Jose David Divad
 --@Fecha:   01/02/2021
 --@Descripcion: Deshabilitar la generación de datos redo en la carga inicial
+
+connect sys/systemproy as sysdba
+
+alter tablespace tst_tramites nologging;
+alter tablespace tsi_tramites nologging;
+alter tablespace tst_ciudadanos nologging;
+alter tablespace tsi_ciudadanos nologging;
+alter tablespace comun_blob nologging;
