55eaf4e37024c75f334a53919c7ba6e8d11cd226
JoseDavidMora <josedavidmoramagana@gmail.com>
2021-02-03 19:38:17 -0600

    insert subtipos de falta

---
+++ DML/s-14-insert-alto.sql
@@ -0,0 +1,39 @@
+--@Autores: Jaime Garcia Frnacisco
+--     	    Mora Magaña Jose David Divad
+--@Fecha:   01/02/2021
+--@Descripcion:  Inserts de la tabla alto (33)
+
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (1, 'Clarendon');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (2, 'Dawn');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (3, 'Washington');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (4, 'Spaight');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (5, 'Jenifer');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (6, 'Center');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (7, 'Fallview');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (8, 'Hauk');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (9, 'Drewry');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (10, 'Nova');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (11, 'Crownhardt');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (12, 'Westport');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (13, 'Johnson');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (14, 'Monument');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (15, 'Garrison');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (16, 'Thackeray');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (17, 'Dayton');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (18, 'Talmadge');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (19, 'Northview');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (20, 'Ohio');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (21, 'Lighthouse Bay');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (22, 'Blackbird');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (23, 'Manufacturers');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (24, 'Carberry');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (25, 'Steensland');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (26, 'Cambridge');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (27, 'Pepper Wood');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (28, 'Kensington');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (29, 'Westridge');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (30, 'Toban');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (31, 'Monument');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (32, 'Hovde');
+insert into ALTO (FALTA_CIVIL_ID, CRUCERO) values (33, 'Northport');
+
