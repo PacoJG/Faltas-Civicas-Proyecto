bd04f7fb92592dafcd3e4d9c0d1da6c1bc96d99b
PacoJG <ing.franciscojaimegarcia@gmail.com>
2021-02-03 23:33:34 -0600

    Creacion de la BD

--- Creacion BD/s-04-crea-bd-ordinario.sql
+++ Creacion BD/s-04-crea-bd-ordinario.sql
@@ -32,24 +32,24 @@ maxdatafiles 1024
 character set AL32UTF8
 national character set AL16UTF16
 extent management local
-datafile '/u01/app/oracle/oradata/jamoproy/system01.dbf'
+datafile '/u01/app/oracle/oradata/jamoproy/TBS/system01.dbf'
 size 700m reuse autoextend on next 10240k maxsize unlimited
-sysaux datafile '/u01/app/oracle/oradata/jamoproy/sysaux01.dbf'
+sysaux datafile '/u01/app/oracle/oradata/jamoproy/TBS/sysaux01.dbf'
 size 550m reuse autoextend on next 10240k maxsize unlimited
 default tablespace users
-datafile '/disk_2/app/oracle/oradata/jamoproy/users01.dbf'
+datafile '/u01/app/oracle/oradata/jamoproy/TBS/users01.dbf'
 size 500m reuse autoextend on maxsize unlimited
 default temporary tablespace tempts1
-tempfile '/disk_3/app/oracle/oradata/jamoproy/temp01.dbf'
+tempfile '/u01/app/oracle/oradata/jamoproy/TBS/temp01.dbf'
 size 20m reuse autoextend on next 640k maxsize unlimited
 undo tablespace undotbs1
-datafile '/disk_3/app/oracle/oradata/jamoproy/undotbs01.dbf'
+datafile '/u01/app/oracle/oradata/jamoproy/TBS/undotbs01.dbf'
 size 200m reuse autoextend on next 5120k maxsize unlimited;
 user_data tablespace usertbs
-datafile '/disk_3/app/oracle/oradata/jamoproy/usertbs01.dbf'
+datafile '/u01/app/oracle/oradata/jamoproy/TBS/usertbs01.dbf'
 size 200m reuse autoextend on maxsize unlimited;
 
 alter user sys identified by systemproy;
 alter user system identified by systemproy;
-
+create pfile from spfile;
 whenever sqlerror continue none;
