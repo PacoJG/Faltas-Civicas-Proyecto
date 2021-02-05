94b87536616936136b4e246deb20e6258fa0d9dc
PacoJG <ing.franciscojaimegarcia@gmail.com>
2021-02-05 03:15:05 -0600

    Modificacion crear tablas

--- Acordeon vistas/acorden.sql
+++ Acordeon vistas/acorden.sql
@@ -1,3 +1,5 @@
 --VER LOS TABLESPACES DELA BASE
 select tablespace_name from dba_tablespace;
 
+--VER EN QUE MODO ENTRASTE A LA BASE
+select sid, username, status, server, osuser, process from v$session where username='SYS';
