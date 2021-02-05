c9ab692c986dff9bf38e600bfdb78579e443825c
PacoJG <ing.franciscojaimegarcia@gmail.com>
2021-02-04 22:41:07 -0600

    Modificación tablespace

--- Creacion BD/s-06-crea-tablespace.sql
+++ Creacion BD/s-06-crea-tablespace.sql
@@ -6,28 +6,32 @@
 connect sys/systemproy as sysdba;
 
 create tablespace comun_blob
-    datafile '/u01/app/oracle/oradata/jamoproy/comun_blob01.dbf' size 100m
+    datafile '/u01/app/oracle/oradata/jamoproy/comun_blob_01.dbf' size 100m,
+             '/u01/app/oracle/oradata/jamoproy/comun_blob_02.dbf' size 100m
     autoextend on next 100m maxsize unlimited
     extent management local autoallocate
     segment space management auto
 ;
 
 create bigfile tablespace tst_tramites
-    datafile '/disk_2/app/oracle/oradata/jamoproy/TBS/tst_tramites.dbf' size 200m
+    datafile '/disk_2/app/oracle/oradata/jamoproy/TBS/tst_tramites_01.dbf' size 300m,
+             '/disk_2/app/oracle/oradata/jamoproy/TBS/tst_tramites_02.dbf' size 300m
     autoextend on next 100m maxsize unlimited
     extent management local autoallocate
     segment space management auto
 ;
 
 create tablespace tsi_tramites
-    datafile '/u01/app/oracle/oradata/jamoproy/TBS/tsi_tramites.dbf' size 100m
+    datafile '/u01/app/oracle/oradata/jamoproy/TBS/tsi_tramites_01.dbf' size 100m,
+             '/u01/app/oracle/oradata/jamoproy/TBS/tsi_tramites_02.dbf' size 100m
     autoextend on next 1280k maxsize unlimited
     extent management local autoallocate
     segment space management auto
 ;
 
 create bigfile tablespace tst_ciudadanos
-    datafile '/disk_3/app/oracle/oradata/jamoproy/TBS/tst_ciudadanos.dbf' size 200m
+    datafile '/disk_3/app/oracle/oradata/jamoproy/TBS/tst_ciudadanos_01.dbf' size 300m,
+             '/disk_3/app/oracle/oradata/jamoproy/TBS/tst_ciudadanos_02.dbf' size 300m
     autoextend on next 100m maxsize unlimited
     encryption using 'aes256' encrypt
     extent management local autoallocate
@@ -35,7 +39,8 @@ create bigfile tablespace tst_ciudadanos
 ;
 
 create tablespace tsi_ciudadanos
-    datafile '/u01/app/oracle/oradata/jamoproy/TBS/tsi_ciudadanos.dbf' size 100m
+    datafile '/u01/app/oracle/oradata/jamoproy/TBS/tsi_ciudadanos_01.dbf' size 100m,
+             '/u01/app/oracle/oradata/jamoproy/TBS/tsi_ciudadanos_02.dbf' size 100m
     autoextend on next 1280k maxsize unlimited
     extent management local autoallocate
     segment space management auto
